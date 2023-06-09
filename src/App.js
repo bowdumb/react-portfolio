264748b40e2151efbf7ebad4e360468445254a47
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-06-08 22:15:04 -0400

    Got a basic nav bar with a header, and the About component renders as the body on load.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
+  const title = 'Josh Claxton: Full-Stack Web Developer';
+  useEffect(() => {
+    document.title = title;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+function Navbar() {
+    return (
+      <div>
+        <section className="navbar">
+          <div className="navbar-content">
+          <h1 style={{ color: 'orange', fontSize: '50px', marginBottom: '10px' }}>Josh Claxton Portfolio</h1>
+            <nav>
+              <div className="navbar-links">
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/resume">Resumé</Link>
+              </div>
+            </nav>
+          </div>
+        </section>
+  
+        {/* Placeholder for content */}
+      </div>
+    );
+  }
+  
+
+export default Navbar;
