0298d0390684fca5e60085286352edfc8ff4079c
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-06-08 22:15:04 -0400

    Got a basic nav bar with a header, and the About component renders as the body on load.

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/About.css';
+
+function About() {
+  return (
+    <div>
+      <section className="about-me">
+        <h1>About Me</h1>
+        {/* Additional content */}
+        <p>This is the About page content.</p>
+      </section>
+    </div>
+  );
+}
+
+export default About;
