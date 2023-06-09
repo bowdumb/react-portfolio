6f3e7586876f9f248bec254f847977d0d0579269
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-06-08 22:45:51 -0400

    Added text content to the About component.

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,20 @@ function About() {
     <div>
       <section className="about-me">
         <h1>About Me</h1>
-        {/* Additional content */}
-        <p>This is the About page content.</p>
+        <p>
+          I was born in Washington, DC, but I have been living in Atlanta, Georgia for the past two years.
+          For the last decade, I worked in the wine and hospitality industries, specifically as a sommelier in New York City.
+          Recently, I made the decision to transition my career in order to pursue my passion for technology.
+          I enrolled in and successfully completed a full-stack coding bootcamp through Georgia Tech's professional education program.
+        </p>
+        <p>
+          Throughout the bootcamp, I gained proficiency in various technologies and programming languages, including JavaScript, CSS, HTML, Express, MySQL, MongoDB, GraphQL, React, and more.
+          I am excited to leverage these skills and embark on a new journey as a web developer.
+        </p>
+        <p>
+          I am currently seeking employment opportunities that will allow me to continue learning and growing as a web developer.
+          I am eager to contribute my knowledge and skills to create innovative and user-friendly web solutions.
+        </p>
       </section>
     </div>
   );
