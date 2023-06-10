f7c4ae61c64c593796b39efb73e10f9930c175dd
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-06-10 17:13:26 -0400

    Added icons for social media links and included change in color when hovering.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import '../styles/Footer.css';
 
 function Footer() {
     return (
         <footer className="footer">
-            <div classname="footer-content">
-                <p>Footer content placeholder.</p>
+            <div className="footer-content">
+                <a href="https://github.com/claxCode">
+                <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="https://www.linkedin.com/in/joshua-claxton-916a2a272/">
+                <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a href="https://twitter.com/bowdumb">
+                <FontAwesomeIcon icon={faTwitter} />
+                </a>
             </div>
         </footer>
             
